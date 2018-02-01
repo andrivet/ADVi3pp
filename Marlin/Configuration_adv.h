55d3ddd9d5c1fc8e1d1b9a1f4dd0e1eb041672d7
Sebastien Andrivet <sebastien@advtools.com>
2018-02-01 22:05:42 +0100

    Use MAX_CONSECUTIVE_LOW_TEMPERATURE_ERROR_ALLOWED. Some people do have sudden drop in temperature.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -147,7 +147,7 @@
 
 // The number of consecutive low temperature errors that can occur
 // before a min_temp_error is triggered. (Shouldn't be more than 10.)
-//#define MAX_CONSECUTIVE_LOW_TEMPERATURE_ERROR_ALLOWED 0
+#define MAX_CONSECUTIVE_LOW_TEMPERATURE_ERROR_ALLOWED 5
 
 // The number of milliseconds a hotend will preheat before starting to check
 // the temperature. This value should NOT be set to the time it takes the
