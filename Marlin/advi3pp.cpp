db750cb9a28ddc10cd4d77111307ee2417cc242c
Sebastien Andrivet <sebastien@advtools.com>
2018-06-03 23:00:36 +0200

    Bump version

--- Marlin/advi3pp.cpp
+++ Marlin/advi3pp.cpp
@@ -43,9 +43,8 @@
 namespace
 {
     const uint16_t advi3_pp_version = 0x302;
-    const uint16_t advi3_pp_oldest_lcd_compatible_version = 0x300;
+    const uint16_t advi3_pp_oldest_lcd_compatible_version = 0x302;
     const uint16_t advi3_pp_newest_lcd_compatible_version = 0x302;
-    // Modify also DETAILED_BUILD_VERSION in Version.h
 
     const unsigned long advi3_pp_baudrate = 115200; // Between the LCD panel and the mainboard
     const uint16_t nb_visible_sd_files = 5;
