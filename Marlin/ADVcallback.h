3f1f0db4a2380e670cdb32f94bd30d7fa1fa4832
Sebastien Andrivet <sebastien@andrivet.com>
2018-09-15 17:30:46 +0200

    Enhance Callback classes: use unique_ptr, remove unnecessary template parameter M.

--- Marlin/ADVcallback.h
+++ Marlin/ADVcallback.h
@@ -29,58 +29,61 @@ namespace andrivet
 template<typename R, typename...A>
 struct Callable
 {
-    virtual Callable* clone() const = 0;
+    virtual unique_ptr<Callable> clone() const = 0;
     virtual R operator()(A...args) const = 0;
     virtual ~Callable() = default;
 };
 
-template<typename F, typename R, typename...A>
+template<typename FP, typename R, typename...A>
 struct CallableFunction: public Callable<R, A...>
 {
     using Super = Callable<R, A...>;
-    using Self = CallableFunction<F, R, A...>;
+    using Self = CallableFunction<FP, R, A...>;
 
-    explicit CallableFunction(F f): function_{f} {}
-    Self* clone() const override { return new CallableFunction(function_); }
+    explicit CallableFunction(FP f): function_{f} {}
+    unique_ptr<Super> clone() const override { return make_unique<Self>(function_); }
 
-    R operator()(A...args) const override { if(is_void<R>::value) function_(args...); else return function_(args...); }
+    R operator()(A...args) const override { if(is_void<R>::value) function_(args...);
+        else return function_(args...); }
 
 private:
-    F function_;
+    FP function_; // In fact, pointer to function
 };
 
 template<typename O, typename R, typename...A>
 struct CallableMethod: public Callable<R, A...>
 {
-    using M = R(O::*)(A...);
+    using MP = R(O::*)(A...); // Pointer to member function type
     using Super = Callable<R, A...>;
     using Self = CallableMethod<O, R, A...>;
 
-    CallableMethod(O& o, M m): object_{o}, method_{m} {}
-    Self* clone() const override { return new Self(object_, method_); }
+    CallableMethod(O& o, const MP m): object_{o}, method_{m} {}
+    unique_ptr<Super> clone() const override { return make_unique<Self>(object_, method_); }
 
-    R operator()(A...args) const override { if(is_void<R>::value) (object_.*method_)(args...); else return (object_.*method_)(args...); }
+    R operator()(A...args) const override { if(is_void<R>::value) (object_.*method_)(args...);
+        else return (object_.*method_)(args...); }
 
 private:
     O& object_;
-    M method_;
+    const MP method_; // In fact, pointer to member function
 };
 
 template<typename O, typename R, typename...A>
 struct CallableConstMethod: public Callable<R, A...>
 {
-    using M = R(O::*)(A...);
+    using MP = R(O::*)(A...); // Pointer to member function type
     using Super = Callable<R, A...>;
     using Self = CallableMethod<O, R, A...>;
 
-    CallableConstMethod(const O& o, M m): object_{o}, method_{m} {}
-    Self* clone() const override { return new Self(object_, method_); }
+    CallableConstMethod(const O& o, const MP m): object_{o}, method_{m} {}
+    unique_ptr<Super> clone() const override { return make_unique<Self>(object_, method_); }
 
-    R operator()(A...args) const override { if(is_void<R>::value) (object_.*method_)(args...); else return (object_.*method_)(args...); }
+    R operator()(A...args) const override { if(is_void<R>::value) (object_.*method_)(args...);
+        else return (object_.*method_)(args...); }
 
 private:
     const O& object_;
-    M method_;
+    const MP method_; // In fact, pointer to member function
 };
 
 template<typename>
@@ -89,46 +92,48 @@ struct Callback;
 template <typename R, typename... A>
 struct Callback<R(*)(A...)>
 {
-    using F = R(*)(A...);
-    using CB = Callback<F>;
+    using FP = R(*)(A...);
+    using Self = Callback<R(*)(A...)>;
 
     // Empty callback
     Callback() noexcept = default;
     explicit Callback(nullptr_t) noexcept {}
 
     // From a function
-    explicit Callback(F f): callable_{new CallableFunction<F, R, A...>(f)} {}
+    explicit Callback(FP f): callable_{make_unique<CallableFunction<FP, R, A...>>(f)} {}
 
-    // From a member function
-    template <typename O, typename M>
-    Callback(O& o, const M& m): callable_{new CallableMethod<O, R, A...>(o, m)} {}
+    // From a member function and object reference
+    template <typename O>
+    Callback(O& o, R(O::*m)(A...)): callable_{make_unique<CallableMethod<O, R, A...>>(o, m)} {}
 
-    // From a const member function
-    template <typename O, typename M>
-    Callback(const O& o, const M& m): callable_{new CallableConstMethod<O, R, A...>(o, m)} {}
+    // From a member function and object pointer
+    template <typename O>
+    Callback(O* o, R(O::*m)(A...)): callable_{make_unique<CallableMethod<O, R, A...>>(*o, m)} {}
 
-    // From a member function
-    template <typename O, typename M>
-    Callback(O* o, const M& m): callable_{new CallableMethod<O, R, A...>(*o, m)} {}
+    // From a const member function and object reference
+    template <typename O>
+    Callback(const O& o, R(O::*m)(A...)): callable_{make_unique<CallableConstMethod<O, R, A...>>(o, m)} {}
 
-    // From a const member function
-    template <typename O, typename M>
-    Callback(const O* o, const M& m): callable_{new CallableConstMethod<O, R, A...>(*o, m)} {}
+    // From a const member function and object pointer
+    template <typename O>
+    Callback(const O* o, R(O::*m)(A...)): callable_{make_unique<CallableConstMethod<O, R, A...>>(*o, m)} {}
 
     // Captured lambda specialization
     template<typename L>
-    explicit Callback(const L& l): callable_{new CallableFunction<decltype(l), R, A...>(l)} {}
+    explicit Callback(const L& l): callable_{make_unique<CallableFunction<decltype(l), R, A...>>(l)} {}
 
     // From another Callback
-    Callback(const CB& cb): callable_{cb.callable_ != nullptr ? cb.callable_->clone() : nullptr} {}
+    Callback(const Self& cb): callable_{ cb ? cb.callable_->clone() : nullptr } {}
 
     // Assignment
-    Callback& operator=(const CB& c) { callable_.reset(c.callable_ != nullptr ? c.callable_->clone() : nullptr); return *this; }
+    Callback& operator=(const Self& cb) { if(&cb != this) { callable_ = cb ? cb.callable_->clone() : nullptr; }
+        return *this; };
     Callback& operator=(nullptr_t) { callable_ = nullptr; return *this; }
 
     // Call
     R operator()(A... args)
-    { if(is_void<R>::value) { if(callable_) (*callable_)(args...); } else { return callable_ ? (*callable_)(args...) : R(); } }
+        { if(is_void<R>::value) { if(callable_) (*callable_)(args...); }
+          else { return callable_ ? (*callable_)(args...) : R(); } }
 
     // Boolean
     explicit operator bool() const noexcept { return callable_ != nullptr; }
