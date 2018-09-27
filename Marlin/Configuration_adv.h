09b09fb00a489623281be21f285495de5c49ae81
churd83 <churd83@gmail.com>
2018-09-27 15:38:27 -0400

    Inductive Probe Configuration
    Configuration for Stock Probe location of the Mark2, also using 3 probe averaging. Included Configuration_adv.h because I'm not sure if I changed anything within this file.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -351,8 +351,8 @@
 // Homing hits each endstop, retracts by these distances, then does a slower bump.
 #define X_HOME_BUMP_MM 5
 #define Y_HOME_BUMP_MM 5
-#define Z_HOME_BUMP_MM 2
-#define HOMING_BUMP_DIVISOR { 2, 2, 4 }  // Re-Bump Speed Divisor (Divides the Homing Feedrate)
+#define Z_HOME_BUMP_MM 5
+#define HOMING_BUMP_DIVISOR { 2, 2, 1 }  // Re-Bump Speed Divisor (Divides the Homing Feedrate)
 //#define QUICK_HOME                     // If homing includes X and Y, do a diagonal move initially
 
 // When G28 is called, this option will make Y home before X
