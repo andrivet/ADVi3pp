83f6319564df526e677de0109ea8e4de37fdc18f
Sebastien Andrivet <sebastien@andrivet.com>
2018-10-07 14:59:20 +0200

    Adjust copyright year

--- Marlin/advi3pp.cpp
+++ Marlin/advi3pp.cpp
@@ -149,7 +149,7 @@ void ADVi3pp_::show_boot_page()
 //! Under GPLv3 provision 7(b), you are not authorized to remove or alter this notice.
 void ADVi3pp_::send_gplv3_7b_notice()
 {
-    SERIAL_ECHOLNPGM("Based on ADVi3++, Copyright (C) 2017 Sebastien Andrivet");
+    SERIAL_ECHOLNPGM("Based on ADVi3++, Copyright (C) 2017-2018 Sebastien Andrivet");
 }
 
 void ADVi3pp_::send_sponsors()
