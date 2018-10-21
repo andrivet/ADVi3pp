91f811916fd7755b9f57dfd7c14de7fb1ed66ea9
Sebastien Andrivet <sebastien@andrivet.com>
2018-10-21 14:13:56 +0200

    itoa and co are C functions (i.e. C linkage)

--- Unit-Tests/vendors/avr/avrlib.h
+++ Unit-Tests/vendors/avr/avrlib.h
@@ -5,11 +5,13 @@
 #ifndef UNIT_TESTS_AVRLIB_H
 #define UNIT_TESTS_AVRLIB_H
 
-char* itoa(int value, char* str, int base);
-char* utoa(unsigned int value, char* str, int base);
-char* ltoa(long value, char* str, int base);
-char* ultoa(unsigned long value, char* str, int base);
-char* dtostrf(double val, signed char width, unsigned char prec, char* s);
-
+extern "C"
+{
+char *itoa(int value, char *str, int base);
+char *utoa(unsigned int value, char *str, int base);
+char *ltoa(long value, char *str, int base);
+char *ultoa(unsigned long value, char *str, int base);
+char *dtostrf(double val, signed char width, unsigned char prec, char *s);
+};
 
 #endif //UNIT_TESTS_AVRLIB_H
