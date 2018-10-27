1a41676bba9d9a1c63079b3f9acbeb6d8e5647f9
Sebastien Andrivet <sebastien@andrivet.com>
2018-10-27 17:01:20 +0200

    Lower the brightness when dimmed

--- Marlin/advi3pp_handlers.cpp
+++ Marlin/advi3pp_handlers.cpp
@@ -55,7 +55,7 @@ namespace
 
     const int8_t BRIGHTNESS_MIN = 0x01;
     const int8_t BRIGHTNESS_MAX = 0x40;
-    const uint8_t DIMMING_RATIO = 25; // in percent
+    const uint8_t DIMMING_RATIO = 5; // in percent
     const uint16_t DIMMING_DELAY = 1 * 60;
 
     const advi3pp::Preset DEFAULT_PREHEAT_PRESET[5] = {
