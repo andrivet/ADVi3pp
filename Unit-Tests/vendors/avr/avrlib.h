1c26914d91a882db49c6424cd99cc8990ef4a112
Sebastien Andrivet <sebastien@andrivet.com>
2018-11-12 18:43:14 +0100

    Merge upstream

--- Unit-Tests/vendors/avr/avrlib.h
+++ Unit-Tests/vendors/avr/avrlib.h
@@ -7,11 +7,11 @@
 
 extern "C"
 {
-char *itoa(int value, char *str, int base);
-char *utoa(unsigned int value, char *str, int base);
-char *ltoa(long value, char *str, int base);
-char *ultoa(unsigned long value, char *str, int base);
-char *dtostrf(double val, signed char width, unsigned char prec, char *s);
+char* itoa(int value, char* str, int base);
+char* utoa(unsigned int value, char* str, int base);
+char* ltoa(long value, char* str, int base);
+char* ultoa(unsigned long value, char* str, int base);
+char* dtostrf(double val, signed char width, unsigned char prec, char* s);
 };
 
 #endif //UNIT_TESTS_AVRLIB_H
