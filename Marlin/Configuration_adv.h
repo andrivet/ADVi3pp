418027b5eedf16e98994f49667979b4ac4684f72
Sebastien Andrivet <sebastien@andrivet.com>
2019-05-20 21:34:12 +0200

    Were too big

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -884,16 +884,16 @@
 // The number of linear motions that can be in the plan at any give time.
 // THE BLOCK_BUFFER_SIZE NEEDS TO BE A POWER OF 2 (e.g. 8, 16, 32) because shifts and ors are used to do the ring-buffering.
 #if ENABLED(SDSUPPORT)
-  #define BLOCK_BUFFER_SIZE 32 // SD,LCD,Buttons take more memory, block buffer needs to be smaller
+  #define BLOCK_BUFFER_SIZE 16 // SD,LCD,Buttons take more memory, block buffer needs to be smaller
 #else
-  #define BLOCK_BUFFER_SIZE 32 // maximize block buffer
+  #define BLOCK_BUFFER_SIZE 16 // maximize block buffer
 #endif
 
 // @section serial
 
 // The ASCII buffer for serial input
 #define MAX_CMD_SIZE 96
-#define BUFSIZE 32 // @advi3++ ADVi3++ needs more than just 4 commands and there is enough free SRAM
+#define BUFSIZE 8 // @advi3++ ADVi3++ needs more than just 4 commands and there is enough free SRAM
 
 // Transmission to Host Buffer Size
 // To save 386 bytes of PROGMEM (and TX_BUFFER_SIZE+3 bytes of RAM) set to 0.
