1c568580aff75f9e2619a2335c460e4a2c0e0af6
Sebastien Andrivet <sebastien@andrivet.com>
2020-01-11 18:00:22 +0100

    Make a microSD image to enter LCD calibration mode

--- LCD-Panel/Scripts/create-sd-calibration-image.sh
+++ LCD-Panel/Scripts/create-sd-calibration-image.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 : '
-Create a microSD disk image for the LCD Panel.
+Create a microSD disk image for the calibration of the LCD Panel.
 '
 
 if [[ "$OSTYPE" != "darwin"* ]]; then echo "Work only on macOS, sorry" ; exit 1; fi
@@ -8,5 +8,4 @@ if [[ $# -ne 1 ]] ; then echo "Please provide a version number such as 1.0.0" ; 
 
 version=$1
 
-./create-sd-image-from-dir.sh "${version}" "DGUS-root" "ADVI3PP" "ADVi3pp-LCD" 1
-
+./create-sd-image-from-dir.sh "${version}" "Calibration" "ADVCALIB" "ADVi3pp-CALIB-LCD" 0
