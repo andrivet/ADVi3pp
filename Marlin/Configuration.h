a3ee4f197987dcadf3192ad365a6f549811cd5a2
Sebastien Andrivet <sebastien@andrivet.com>
2020-01-14 21:50:32 +0100

    Adjust slightly x offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -975,8 +975,8 @@
 #define Y_BED_SIZE 200
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-// @advi3++: The bed dimensions of Wanhao i3 Plus is 200x200x180, X offset is -6.5
-#define X_MIN_POS -6.5
+// @advi3++: The bed dimensions of Wanhao i3 Plus is (officially) 200x200x180
+#define X_MIN_POS -5.0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_MIN_POS + X_BED_SIZE
