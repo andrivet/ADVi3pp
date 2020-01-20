9debe3098074a7fdf81732018c8f090b353f77ab
Sebastien Andrivet <sebastien@andrivet.com>
2020-01-20 20:18:29 +0100

    I prefer 1 beep

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1025,7 +1025,7 @@
   #define FILAMENT_UNLOAD_PURGE_LENGTH         8  // (mm) An unretract is done, then this length is purged.
 
   #define PAUSE_PARK_NOZZLE_TIMEOUT           45  // (seconds) Time limit before the nozzle is turned off for safety.
-  #define FILAMENT_CHANGE_ALERT_BEEPS         10  // Number of alert beeps to play when a response is needed.
+  #define FILAMENT_CHANGE_ALERT_BEEPS          1  // Number of alert beeps to play when a response is needed.
   #define PAUSE_PARK_NO_STEPPER_TIMEOUT           // Enable for XYZ steppers to stay powered on during filament change.
 
   // @advi3++: Park the nozzle on pause and home before changing filament
