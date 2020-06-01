cd5b525a58ba9d693e3c571f009fce7ad1ebc0d9
Sebastien Andrivet <sebastien@andrivet.com>
2020-06-01 18:04:28 +0200

    Implement Feedrate Settings

--- Marlin/src/advi3pp/screens/settings/feedrate_settings.cpp
+++ Marlin/src/advi3pp/screens/settings/feedrate_settings.cpp
@@ -19,9 +19,49 @@
  */
 
 #include "feedrate_settings.h"
+#include "../../core/dgus.h"
 
 namespace ADVi3pp {
 
 FeedrateSettings feedrates_settings;
+
+//! Prepare the page before being displayed and return the right Page value
+//! @return The index of the page to display
+Page FeedrateSettings::do_prepare_page()
+{
+    WriteRamDataRequest frame{Variable::Value0};
+    frame << Uint16(ExtUI::getAxisMaxFeedrate_mm_s(ExtUI::X))
+          << Uint16(ExtUI::getAxisMaxFeedrate_mm_s(ExtUI::Y))
+          << Uint16(ExtUI::getAxisMaxFeedrate_mm_s(ExtUI::Z))
+          << Uint16(ExtUI::getAxisMaxFeedrate_mm_s(ExtUI::E0))
+          << Uint16(ExtUI::getMinFeedrate_mm_s())
+          << Uint16(ExtUI::getMinTravelFeedrate_mm_s());
+    frame.send();
+
+    return Page::FeedrateSettings;
+}
+
+//! Save the Feedrate settings
+void FeedrateSettings::do_save_command()
+{
+    ReadRamData response{Variable::Value0, 6};
+    if(!response.send_and_receive())
+    {
+        Log::error() << F("Receiving Frame (Feedrate Settings)") << Log::endl();
+        return;
+    }
+
+    Uint16 x, y, z, e, min, travel;
+    response >> x >> y >> z >> e >> min >> travel;
+
+    ExtUI::setAxisMaxFeedrate_mm_s(static_cast<float>(x.word), ExtUI::X);
+    ExtUI::setAxisMaxFeedrate_mm_s(static_cast<float>(y.word), ExtUI::Y);
+    ExtUI::setAxisMaxFeedrate_mm_s(static_cast<float>(z.word), ExtUI::Z);
+    ExtUI::setAxisMaxFeedrate_mm_s(static_cast<float>(e.word), ExtUI::E0);
+    ExtUI::setMinFeedrate_mm_s(static_cast<float>(min.word));
+    ExtUI::setMinTravelFeedrate_mm_s(static_cast<float>(travel.word));
+
+    Parent::do_save_command();
+}
 
 }
