02fbfe65e0918f57b48cf9de19aa9832e368ec61
Sebastien Andrivet <sebastien@andrivet.com>
2020-06-01 18:06:28 +0200

    Implement Sensor Tuning

--- Marlin/src/advi3pp/screens/tuning/sensor_tuning.cpp
+++ Marlin/src/advi3pp/screens/tuning/sensor_tuning.cpp
@@ -24,4 +24,71 @@ namespace ADVi3pp {
 
 SensorTuning sensor_tuning;
 
+
+#ifdef ADVi3PP_PROBE
+
+//! Execute a Sensor Tuning command
+//! @param key_value    The sub-action to handle
+//! @return             True if the action was handled
+bool SensorTuning::do_dispatch(KeyValue key_value)
+{
+    if(Parent::do_dispatch(key_value))
+        return true;
+
+    switch(key_value)
+    {
+        case KeyValue::SensorSelfTest:  self_test_command(); break;
+        case KeyValue::SensorReset:     reset_command(); break;
+        case KeyValue::SensorDeploy:    deploy_command(); break;
+        case KeyValue::SensorStow:      stow_command(); break;
+        default:                        return false;
+    }
+
+    return true;
+}
+
+//! Prepare the page before being displayed and return the right Page value
+//! @return The index of the page to display
+Page SensorTuning::do_prepare_page()
+{
+    if(!print.ensure_not_printing())
+        return Page::None;
+    return Page::SensorTuning;
+}
+
+//! Execute the sensor Self-test command
+void SensorTuning::self_test_command()
+{
+    enqueue_and_echo_commands_P(PSTR("M280 P0 S120"));
+}
+
+//! Execute the sensor Reset command
+void SensorTuning::reset_command()
+{
+    enqueue_and_echo_commands_P(PSTR("M280 P0 S160"));
+}
+
+//! Execute the sensor Deploy command
+void SensorTuning::deploy_command()
+{
+    enqueue_and_echo_commands_P(PSTR("M280 P0 S10"));
+}
+
+//! Execute the sensor Stow command
+void SensorTuning::stow_command()
+{
+    enqueue_and_echo_commands_P(PSTR("M280 P0 S90"));
+}
+
+#else
+
+//! Prepare the page before being displayed and return the right Page value
+//! @return The index of the page to display
+Page SensorTuning::do_prepare_page()
+{
+    return Page::NoSensor;
+}
+
+#endif
+
 }
