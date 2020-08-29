23395d12a0422027bca9ef4486986ad038a569d7
Sebastien Andrivet <sebastien@andrivet.com>
2020-08-29 20:42:39 +0200

    No valid mesh is not fatal when trying to activate it

--- Marlin/src/gcode/bedlevel/M420.cpp
+++ Marlin/src/gcode/bedlevel/M420.cpp
@@ -221,8 +221,9 @@ void GcodeSuite::M420() {
   #endif
 
   // Error if leveling failed to enable or reenable
-  if (to_enable && !planner.leveling_active)
-    SERIAL_ERROR_MSG(STR_ERR_M420_FAILED);
+  // @advi3++: This is not fatal
+  /*if (to_enable && !planner.leveling_active)
+    SERIAL_ERROR_MSG(STR_ERR_M420_FAILED);*/
 
   SERIAL_ECHO_START();
   SERIAL_ECHOPGM("Bed Leveling ");
