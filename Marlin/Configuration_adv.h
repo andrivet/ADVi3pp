c3cb408a14bfd55ebd5fa1669abf9467e89bfe0d
Sebastien Andrivet <sebastien@andrivet.com>
2021-05-29 14:23:30 +0200

    Set same value than in v4

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -218,7 +218,7 @@
 #if ENABLED(THERMAL_PROTECTION_BED)
   // @advi3++: Use appropriate values for Wanaho i3 Plus
   #define THERMAL_PROTECTION_BED_PERIOD        30 // Seconds
-  #define THERMAL_PROTECTION_BED_HYSTERESIS     4 // Degrees Celsius
+  #define THERMAL_PROTECTION_BED_HYSTERESIS     3 // Degrees Celsius
 
   /**
    * As described above, except for the bed (M140/M190/M303).
