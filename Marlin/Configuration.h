e8a7307d3f05b18c4ff15f6d1a981850b8290615
Sebastien Andrivet <sebastien@andrivet.com>
2021-09-14 18:43:04 +0200

    Fix wrong size of the bed.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1183,11 +1183,11 @@
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 // @advi3++: The bed dimensions of Wanhao i3 Plus is (officially) 200x200x180
-#define X_MIN_POS -5.0
+#define X_MIN_POS -2.0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_MIN_POS + X_BED_SIZE
-#define Y_MAX_POS Y_MIN_POS + Y_BED_SIZE
+#define X_MAX_POS X_BED_SIZE
+#define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 180
 
 /**
