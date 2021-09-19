944cd7326af45005f8de685928d942b48df4a6cd
Sebastien Andrivet <sebastien@andrivet.com>
2021-09-19 18:13:24 +0200

    Be sure images are always there.

--- Scripts/create-sd-image.sh
+++ Scripts/create-sd-image.sh
@@ -8,5 +8,13 @@ if [[ $# -ne 1 ]] ; then echo "Please provide a version number such as 1.0.0" ; 
 
 version=$1
 
+echo
+echo "***** Convert images..."
+echo
+./convert-boot-images.sh
+ret=$?; if [[ $ret != 0 ]]; then exit $ret; fi
+./convert-images.sh
+ret=$?; if [[ $ret != 0 ]]; then exit $ret; fi
+
 ./create-sd-image-from-dir.sh "${version}" "DGUS-root" "ADVI3PP" "ADVi3pp-LCD" 1
 
