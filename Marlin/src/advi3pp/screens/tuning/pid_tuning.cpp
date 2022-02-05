f3af3d3c9fbb4915eef731010a9ae84ed8947665
Sebastien Andrivet <sebastien@andrivet.com>
2022-02-05 14:35:29 +0100

    Avoid compilation warning

--- Marlin/src/advi3pp/screens/tuning/pid_tuning.cpp
+++ Marlin/src/advi3pp/screens/tuning/pid_tuning.cpp
@@ -100,7 +100,7 @@ void PidTuning::step2_command()
     }
 
     temperature_ = frame.read_word();
-    uint16_t  kind = frame.read_word();
+    [[maybe_unused]] uint16_t  kind = frame.read_word();
     assert(static_cast<TemperatureKind>(kind) == kind_);
 
     state_ |= State::FromLCDMenu;
