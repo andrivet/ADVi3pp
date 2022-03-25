d84f61713394b96b00c7a0c35b7cface14b71091
Sebastien Andrivet <sebastien@andrivet.com>
2022-03-25 11:08:39 +0100

    Fix rounding and wrong int types (#268)

--- Marlin/src/advi3pp/screens/settings/sensor_settings.cpp
+++ Marlin/src/advi3pp/screens/settings/sensor_settings.cpp
@@ -133,19 +133,21 @@ void SensorSettings::next_command()
 
 void SensorSettings::send_values() const
 {
+    int16_t z_offset = LROUND(ExtUI::getZOffset_mm() * 100.0f);
+
     if(index_ == 0)
         WriteRamRequest{Variable::Value0}.write_words(adv::array<uint16_t, 3>
         {
-            static_cast<uint16_t>(ExtUI::getProbeOffset_mm(ExtUI::X) * 100),
-            static_cast<uint16_t>(ExtUI::getProbeOffset_mm(ExtUI::Y) * 100),
-            static_cast<uint16_t>(ExtUI::getZOffset_mm() * 100)
+            static_cast<uint16_t>(ExtUI::getProbeOffset_mm(ExtUI::X) * 100.0f),
+            static_cast<uint16_t>(ExtUI::getProbeOffset_mm(ExtUI::Y) * 100.0f),
+            static_cast<uint16_t>(z_offset)
         });
     else
         WriteRamRequest{Variable::Value0}.write_words(adv::array<uint16_t, 3>
         {
             static_cast<uint16_t>(SENSOR_POSITION[index_ - 1].x),
             static_cast<uint16_t>(SENSOR_POSITION[index_ - 1].y),
-            static_cast<uint16_t>(ExtUI::getZOffset_mm() * 100)
+            static_cast<uint16_t>(z_offset)
         });
 }
 
@@ -167,11 +169,11 @@ void SensorSettings::get_values()
 
     int16_t x = frame.read_signed_word();
     int16_t y = frame.read_signed_word();
-    uint16_t z = frame.read_word();
+    int16_t z = frame.read_signed_word();
 
-    ExtUI::setProbeOffset_mm(x / 100.0, ExtUI::X);
-    ExtUI::setProbeOffset_mm(y / 100.0, ExtUI::Y);
-    ExtUI::setZOffset_mm(z / 100.0);
+    ExtUI::setProbeOffset_mm(x / 100.0f, ExtUI::X);
+    ExtUI::setProbeOffset_mm(y / 100.0f, ExtUI::Y);
+    ExtUI::setZOffset_mm(z / 100.0f);
 }
 
 #else
