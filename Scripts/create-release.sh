4fdb2b2a419aa2d5b72e601edaf44a248b12c154
Sebastien Andrivet <sebastien@andrivet.com>
2022-06-12 16:51:08 +0200

    Dev version

--- Scripts/create-release.sh
+++ Scripts/create-release.sh
@@ -9,7 +9,7 @@ function pause(){
    read -r -s -k "?$*"$'\n'
 }
 
-version="5.3.0"
+version="5.4.0-dev"
 
 scripts="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ret=$?; if [[ $ret != 0 ]]; then exit $ret; fi
