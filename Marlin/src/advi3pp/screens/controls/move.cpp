399978f2e393597fe9c83bcb915200b183fe637f
Sebastien Andrivet <sebastien@andrivet.com>
2022-07-07 18:22:01 +0200

    Change the way move commands are executed, it is now asynchronous

--- Marlin/src/advi3pp/screens/controls/move.cpp
+++ Marlin/src/advi3pp/screens/controls/move.cpp
@@ -25,9 +25,11 @@
 namespace ADVi3pp {
 
 namespace {
-  const millis_t MOVE_XY_DELAY = 150;
-  const millis_t MOVE_Z_DELAY = 50;
-  const millis_t MOVE_E_DELAY = 500;
+  const millis_t   CLICK_DELAY = 200;
+  const float      OFFSET_X = 5;
+  const float      OFFSET_Y = 5;
+  const float      OFFSET_Z = 0.5;
+  const float      OFFSET_E = 1;
   const feedRate_t FEEDRATE_X = 16;
   const feedRate_t FEEDRATE_Y = 16;
   const feedRate_t FEEDRATE_Z = 4;
@@ -35,6 +37,73 @@ namespace {
 }
 
 Move move;
+
+ENABLE_BITMASK_OPERATOR(Move::Direction);
+
+// Do not use a struct table because it will take RAM space. So use fonctions instead.
+
+float Move::get_min() const {
+  switch(direction_ & ~Direction::MINUS) {
+    case Direction::X_PLUS: return X_MIN_BED;
+    case Direction::Y_PLUS: return Y_MIN_BED;
+    case Direction::Z_PLUS: return Z_MIN_POS;
+    case Direction::E_PLUS: return -EXTRUDE_MAXLENGTH;
+    default: Log::log() << "Invalid Direction: " << static_cast<uint8_t>(direction_) << Log::endl(); return 0;
+  }
+}
+
+float Move::get_max() const {
+  switch(direction_ & ~Direction::MINUS) {
+    case Direction::X_PLUS: return X_MAX_BED;
+    case Direction::Y_PLUS: return Y_MAX_BED;
+    case Direction::Z_PLUS: return Z_MAX_POS;
+    case Direction::E_PLUS: return EXTRUDE_MAXLENGTH;
+    default: Log::log() << "Invalid Direction: " << static_cast<uint8_t>(direction_) << Log::endl(); return 0;
+  }
+}
+
+float Move::get_offset() const {
+  bool minus = test_one_bit(direction_, Direction::MINUS);
+  switch(direction_ & ~Direction::MINUS) {
+    case Direction::X_PLUS: return minus ? -OFFSET_X : OFFSET_X;
+    case Direction::Y_PLUS: return minus ? -OFFSET_Y : OFFSET_Y;
+    case Direction::Z_PLUS: return minus ? -OFFSET_Z : OFFSET_Z;
+    case Direction::E_PLUS: return minus ? -OFFSET_E : OFFSET_E;
+    default: Log::log() << "Invalid Direction: " << static_cast<uint8_t>(direction_) << Log::endl(); return 0;
+  }
+}
+
+feedRate_t Move::get_feedrate() const {
+  switch(direction_ & ~Direction::MINUS) {
+    case Direction::X_PLUS: return FEEDRATE_X;
+    case Direction::Y_PLUS: return FEEDRATE_Y;
+    case Direction::Z_PLUS: return FEEDRATE_Z;
+    case Direction::E_PLUS: return FEEDRATE_E;
+    default: Log::log() << "Invalid Direction: " << static_cast<uint8_t>(direction_) << Log::endl(); return 0;
+  }
+}
+
+float Move::getPosition() const {
+  switch(direction_ & ~Direction::MINUS) {
+    case Direction::X_PLUS: return ExtUI::getAxisPosition_mm(ExtUI::X);
+    case Direction::Y_PLUS: return ExtUI::getAxisPosition_mm(ExtUI::Y);
+    case Direction::Z_PLUS: return ExtUI::getAxisPosition_mm(ExtUI::Z);
+    case Direction::E_PLUS: return ExtUI::getAxisPosition_mm(ExtUI::E0);
+    default: Log::log() << "Invalid Direction: " << static_cast<uint8_t>(direction_) << Log::endl(); return 0;
+  }
+}
+
+void Move::setPosition() const {
+  Log::log() << "setPosition" << target_position_ << Log::endl();
+  auto feedrate = get_feedrate();
+  switch(direction_ & ~Direction::MINUS) {
+    case Direction::X_PLUS: ExtUI::setAxisPosition_mm(target_position_, ExtUI::X, feedrate); break;
+    case Direction::Y_PLUS: ExtUI::setAxisPosition_mm(target_position_, ExtUI::Y, feedrate); break;
+    case Direction::Z_PLUS: ExtUI::setAxisPosition_mm(target_position_, ExtUI::Z, feedrate); break;
+    case Direction::E_PLUS: ExtUI::setAxisPosition_mm(target_position_, ExtUI::E0, feedrate); break;
+    default: Log::log() << "Invalid Direction: " << static_cast<uint8_t>(direction_) << Log::endl(); break;
+  }
+}
 
 
 //! Execute a move command
@@ -52,7 +121,7 @@ bool Move::do_dispatch(KeyValue key_value)
         case KeyValue::MoveZHome:           z_home_command(); break;
         case KeyValue::MoveAllHome:         all_home_command(); break;
         case KeyValue::DisableMotors:       disable_motors_command(); break;
-        default:                            return false;
+        default: Log::log() << "Invalid KeyValue" << Log::endl(); return false;
     }
 
     return true;
@@ -65,104 +134,146 @@ Page Move::do_prepare_page()
     if(!core.ensure_not_printing())
         return Page::None;
     ExtUI::finishAndDisableHeaters(); // To circumvent homing problems
+    direction_ = Direction::None;
+    background_task.set(Callback{this, &Move::task}, 50);
     return Page::Move;
 }
 
-//! Move the nozzle. Check that the command is not send too early when multiple move commands are send in a short time
-//! (i.e. when the user keep the button presses)
-void Move::move(ExtUI::axis_t axis, float offset, feedRate_t feedrate, millis_t delay) {
-  if(!ELAPSED(millis(), last_move_time_ + delay))
-    return;
-  ExtUI::setAxisPosition_mm(ExtUI::getAxisPosition_mm(axis) + offset, axis, feedrate);
+millis_t move_delay(float offset, feedRate_t feedrate) {
+  millis_t delay = 1000 * abs(offset) / feedrate;
+  Log::log() << "Time: " << millis() << Log::endl();
+  Log::log() << "Delay: " << delay << Log::endl();
+  return delay;
 }
 
-//! Move the nozzle. Check that the command is not send too early when multiple move commands are send in a short time
-//! (i.e. when the user keep the button presses)
-void Move::move(ExtUI::extruder_t extruder, float offset, feedRate_t feedrate, millis_t delay) {
-  if(!ELAPSED(millis(), last_move_time_ + delay))
-    return;
-  ExtUI::setAxisPosition_mm(ExtUI::getAxisPosition_mm(extruder) + offset, extruder, feedrate);
+void Move::move(Move::Direction movement) {
+  Log::log() << "move " << millis() << Log::endl();
+
+  // First move?
+  if(direction_ != movement) {
+    direction_ = movement;
+
+    float min = get_min();
+    float max = get_max();
+    float offset = get_offset();
+    feedRate_t feedrate = get_feedrate();
+
+    target_position_ = getPosition() + offset;
+    target_position_ = (target_position_ < min) ? min : (target_position_ > max) ? max : target_position_;
+
+    setPosition();
+
+    target_move_time_ = millis() + move_delay(offset, feedrate);
+  }
+
+  last_click_time_ = millis();
 }
 
+void Move::reset_move() {
+  Log::log() << "reset_move" << Log::endl();
+  direction_ = Direction::None;
+}
+
+void Move::task() {
+  // No move to do?
+  if(direction_ == Direction::None)
+    return;
+
+  // No click for too long time?
+  if(ELAPSED(millis(), last_click_time_ + CLICK_DELAY)) {
+    reset_move();
+    return;
+  }
+
+  // Not soon end of move?
+  if(!ELAPSED(millis(), target_move_time_ - 5))
+    return;
+
+  Log::log() << "in task, " << millis() << target_move_time_ << ELAPSED(millis(), target_move_time_) << Log::endl();
+
+  float min = get_min();
+  float max = get_max();
+  float offset = get_offset();
+  feedRate_t feedrate = get_feedrate();
+
+  target_position_ += offset;
+  target_position_ = (target_position_ < min) ? min : (target_position_ > max) ? max : target_position_;
+
+  setPosition();
+
+  target_move_time_ += move_delay(offset, feedrate);
+}
 
 //! Move the nozzle (+X)
-void Move::x_plus_command()
-{
-  move(ExtUI::X, 5, FEEDRATE_X, MOVE_XY_DELAY);
+void Move::x_plus_command() {
+  move(Direction::X_PLUS);
 }
 
 //! Move the nozzle (-X)
-void Move::x_minus_command()
-{
-  move(ExtUI::X, -5, FEEDRATE_X, MOVE_XY_DELAY);
+void Move::x_minus_command() {
+  move(Direction::X_MINUS);
 }
 
 //! Move the nozzle (+Y)
-void Move::y_plus_command()
-{
-  move(ExtUI::Y, 5, FEEDRATE_Y, MOVE_XY_DELAY);
+void Move::y_plus_command() {
+  move(Direction::Y_PLUS);
 }
 
 //! Move the nozzle (-Y)
-void Move::y_minus_command()
-{
-  move(ExtUI::X, -5, FEEDRATE_Y, MOVE_XY_DELAY);
+void Move::y_minus_command() {
+  move(Direction::Y_MINUS);
 }
 
 //! Move the nozzle (+Z)
-void Move::z_plus_command()
-{
-  move(ExtUI::Z, 0.5, FEEDRATE_Z, MOVE_Z_DELAY);
+void Move::z_plus_command() {
+  move(Direction::Z_PLUS);
 }
 
 //! Move the nozzle (-Z)
-void Move::z_minus_command()
-{
-  move(ExtUI::Z, -0.5, FEEDRATE_Z, MOVE_Z_DELAY);
+void Move::z_minus_command() {
+  move(Direction::Z_MINUS);
 }
 
 //! Extrude some filament.
-void Move::e_plus_command()
-{
-  move(ExtUI::E0, 1, FEEDRATE_E, MOVE_E_DELAY);
+void Move::e_plus_command() {
+  move(Direction::E_PLUS);
 }
 
 //! Unextrude some filament.
-void Move::e_minus_command()
-{
-  move(ExtUI::E0, -1, FEEDRATE_E, MOVE_E_DELAY);
+void Move::e_minus_command() {
+  move(Direction::E_MINUS);
 }
 
 //! Disable the motors.
-void Move::disable_motors_command()
-{
-    core.inject_commands(F("M84"));
-    ExtUI::setAllAxisUnhomed();
-    ExtUI::setAllAxisPositionUnknown();
+void Move::disable_motors_command() {
+  reset_move();
+  core.inject_commands(F("M84")); // Disable steppers
+  ExtUI::setAllAxisUnhomed();
+  ExtUI::setAllAxisPositionUnknown();
 }
 
 //! Go to home on the X axis.
-void Move::x_home_command()
-{
-    core.inject_commands(F("G28 X F6000"));
+void Move::x_home_command() {
+  reset_move();
+  core.inject_commands(F("G28 X F6000"));
 }
 
 //! Go to home on the Y axis.
-void Move::y_home_command()
-{
-    core.inject_commands(F("G28 Y F6000"));
+void Move::y_home_command() {
+  reset_move();
+  core.inject_commands(F("G28 Y F6000"));
 }
 
 //! Go to home on the Z axis.
-void Move::z_home_command()
-{
-    core.inject_commands(F("G28 Z F1200"));
+void Move::z_home_command() {
+  reset_move();
+  core.inject_commands(F("G28 Z F1200"));
 }
 
 //! Go to home on all axis.
-void Move::all_home_command()
-{
-    core.inject_commands(F("G28 F6000"));
+void Move::all_home_command() {
+  reset_move();
+  core.inject_commands(F("G28 F6000"));
 }
 
 }
--- Marlin/src/advi3pp/screens/controls/move.h
+++ Marlin/src/advi3pp/screens/controls/move.h
@@ -25,32 +25,53 @@
 namespace ADVi3pp {
 
 //! Move Page
-struct Move: Screen<Move>
-{
-    void x_plus_command();
-    void x_minus_command();
-    void x_home_command();
-    void y_plus_command();
-    void y_minus_command();
-    void y_home_command();
-    void z_plus_command();
-    void z_minus_command();
-    void z_home_command();
-    void e_plus_command();
-    void e_minus_command();
-    void all_home_command();
-    void disable_motors_command();
+struct Move: Screen<Move> {
+  friend Parent;
+
+  void x_plus_command();
+  void x_minus_command();
+  void x_home_command();
+  void y_plus_command();
+  void y_minus_command();
+  void y_home_command();
+  void z_plus_command();
+  void z_minus_command();
+  void z_home_command();
+  void e_plus_command();
+  void e_minus_command();
+  void all_home_command();
+  void disable_motors_command();
 
 private:
-    bool do_dispatch(KeyValue key_value);
-    Page do_prepare_page();
-    void move(ExtUI::axis_t axis, float offset, feedRate_t feedrate, millis_t delay);
-    void move(ExtUI::extruder_t extruder, float offset, feedRate_t feedrate, millis_t delay);
+  enum class Direction: uint8_t {
+    None,
+    X_PLUS,
+    Y_PLUS,
+    Z_PLUS,
+    E_PLUS,
+    MINUS = 0x80,
+    X_MINUS = MINUS + X_PLUS,
+    Y_MINUS = MINUS + Y_PLUS,
+    Z_MINUS = MINUS + Z_PLUS,
+    E_MINUS = MINUS + E_PLUS
+  };
+
+  bool do_dispatch(KeyValue key_value);
+  Page do_prepare_page();
+  void move(Move::Direction movement);
+  void reset_move();
+  float getPosition() const;
+  void setPosition() const;
+  float get_min() const;
+  float get_max() const;
+  float get_offset() const;
+  feedRate_t get_feedrate() const;
+  void task();
 
 private:
-    millis_t last_move_time_ = 0;
-
-    friend Parent;
+  Direction direction_ = Direction::None;
+  millis_t last_click_time_ = 0, target_move_time_ = 0;
+  float target_position_ = 0;
 };
 
 extern Move move;
