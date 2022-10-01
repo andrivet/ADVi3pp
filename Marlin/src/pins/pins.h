eb1dc56b7f89f3a3a43a9d0ad034d0ddf059613f
Sebastien Andrivet <sebastien@andrivet.com>
2022-10-01 12:03:52 +0200

    Add simulator env

--- Marlin/src/pins/pins.h
+++ Marlin/src/pins/pins.h
@@ -295,7 +295,7 @@
 //
 
 #elif MB(ADVI3PP_I3_PLUS_51)
-  #include "advi3pp/pins_ADVI3PP.h"              // ATmega2560                             env:advi3pp_51 env:advi3pp_51_bltouch env:advi3pp_51_bltouch_debug
+  #include "advi3pp/pins_ADVI3PP.h"              // ATmega2560                             env:advi3pp_51 env:advi3pp_51_bltouch env:advi3pp_51_bltouch_debug env:advi3pp_simulator
 #elif MB(ADVI3PP_I3_PLUS_52C)
   #include "advi3pp/pins_ADVI3PP.h"              // ATmega2560                             env:advi3pp_52c env:advi3pp_52c_bltouch
 #elif MB(ADVI3PP_I3_PLUS_54)
